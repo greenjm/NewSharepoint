75f9bf8f197154bade26e7f47a2d2ef9589f7b00
Josh Green <greenjm@rose-hulman.edu>
2016-04-20 19:51:46 +0100

    Initial setup for web application

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+require("./router/routes")(app);
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+var server = app.listen(5000, function() {
+	console.log("Now listening on port 5000. Visit 'localhost:5000' in a browser to view the site.");
+});
