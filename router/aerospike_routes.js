6cf41dcee28337ddfe9971dd6d640f7da130200b
Josh Green <greenjm@rose-hulman.edu>
2016-04-22 18:11:11 +0100

    Fixed aerospike connection issues

--- router/aerospike_routes.js
+++ router/aerospike_routes.js
@@ -1,17 +1,19 @@
 module.exports = function(app, api) {
 	var dbStatusCode = null;
 
-	api.connect(function(response) {
- 		dbStatusCode = response;
- 		if ( dbStatusCode === 0) {
-     		// handle success
-     		console.log("Connection to Aerospike cluster succeeded!");
- 		}
- 		else {
-     		// handle failure
-     		console.log("Connection to Aerospike cluster failed!");
- 		}
-	});
+	var connect = function() {
+		api.connect(function(response) {
+	 		dbStatusCode = response;
+	 		if ( dbStatusCode === 0) {
+	     		// handle success
+	     		console.log("Connection to Aerospike cluster succeeded!");
+	 		}
+	 		else {
+	     		// handle failure
+	     		console.log("Connection to Aerospike cluster failed!");
+	 		}
+		});
+	}();
 
 	app.get("/aerospike/testRoute", function(req, res) {
 		api.readRecord('Hello', function(response) {
@@ -25,7 +27,10 @@ module.exports = function(app, api) {
 	                console.log("FAILURE");
 	            }
 	            res.send(response.message);
+        	} else {
+        		console.log("No connection, trying again");
+        		connect();
         	}
-        }); 
+        });
 	});
 }
