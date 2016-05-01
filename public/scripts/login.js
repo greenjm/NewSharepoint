9542d26afc7c65aa3a55541f12384cd8b597d8c8
Josh Green <greenjm@rose-hulman.edu>
2016-05-01 23:54:50 +0100

    /firebase authentication working

---
+++ public/scripts/login.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	
+	var tryLogin = function() {
+		var email = $("#email").val();
+		var password = $("#pass").val();
+		console.log("email: " + email + ". password: " + password);
+
+		var data = {
+			email: email,
+			password: password
+		};
+
+		console.log("data: " + data.email);
+
+		$.ajax({
+			url: "/login/firebase",
+			type: "POST",
+			data: data,
+			success: function(data) {
+				console.log("SUCCESS: " + data);
+			},
+			error: function(data) {
+				console.log("ERROR: " + data);
+			}
+		})
+	}
+
+	$("#sign-in").on("click", function() {
+		console.log("CLICKED");
+		tryLogin();
+	});
+
+});
--- router/routes.js
+++ router/routes.js
@@ -1,4 +1,11 @@
 module.exports = function(app) {
+	var Firebase = require("firebase");
+	var bodyParser = require("body-parser");
+	var ref = new Firebase("https://newsharepoint.firebaseio.com/");
+
+	var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+	// GET
 	app.get("/", function(req, res) {
 		res.render("index.html");
 	});
@@ -11,11 +18,29 @@ module.exports = function(app) {
 		res.render("user.html");
 	});
 
-  app.get("/add", function(req, res) {
-    res.render("addpost.html");
-  });
+  	app.get("/add", function(req, res) {
+    	res.render("addpost.html");
+  	});
 
 	app.get("/forum", function(req, res) {
 		res.render("forum.html");
+	});
+
+	//POST
+	app.post("/login/firebase", urlencodedParser, function(req, res) {
+		var email = req.body.email;
+		var password = req.body.password;
+
+		if (email == "" || password == "") {
+			res.status(400).send("Either email or password is blank");
+		}
+
+		ref.authWithPassword(req.body, function(error, authData) {
+			if (error) {
+    			res.status(400).send(error);
+  			} else {
+    			res.status(200).send(authData);
+  			}
+		});
 	});
 }
