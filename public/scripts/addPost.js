6a2bbaa0e6616634374c8a09b7c5a81496c688a3
JJGayso <gaysojj@rose-hulman.edu>
2016-05-01 19:36:47 -0400

    Can add post!

--- public/scripts/addPost.js
+++ public/scripts/addPost.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+
+      tinymce.init({
+      selector: '#mytextarea'
+      });
 	
+
 	var $title = $('#postTitle');
 	var $contents = $("#mytextarea");
 
 	var addEventListeners = function() {
 		$("#submitPost").on("click", function() {
+			console.log("STUFF" + tinymce.activeEditor.getContent());
+			var tempContents = tinymce.activeEditor.getContent();
 			var post = {
 				title: $title.val(),
-				contents: $contents.val()
+				content: tempContents
 			};
 
 			$.ajax({
@@ -15,7 +22,7 @@ $(document).ready(function() {
 				type: 'POST',
 				data: post,
 				success: function(data) {
-					console.log("Success!"]);
+					console.log("Success!");
 				},
 				error: function(data) {
 					console.log("Error");
